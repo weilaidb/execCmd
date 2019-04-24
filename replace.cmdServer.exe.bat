80e711bece13bd5efd5764d91f31cd0da4d72920
weilaidb <wxjlmr@126.com>
2019-04-25 00:04:15 +0800

    修改拷贝脚本文件。

--- replace.cmdServer.exe.bat
+++ replace.cmdServer.exe.bat
@@ -1,6 +1,8 @@
 @echo on
 taskkill /im cmdServer.exe /f 
-copy /Y cmdServer-build-desktop-Qt_4_8_1_for_Desktop_-_MinGW__Qt_SDK____\release\cmdServer.exe D:\Software\Dropbox\yilinsendmail\cmdServer.exe
+taskkill /im execWinCmdServer2.exe /f 
+copy /Y cmdServer-build-desktop-Qt_4_8_1_for_Desktop_-_MinGW__Qt_SDK____\release\cmdServer.exe D:\Dropbox\cmdServer_lib4.8.1_v6.1\
+copy /Y execWinCmdServer2\release\execWinCmdServer2.exe  D:\Dropbox\cmdServer_lib4.8.1_v6.1\
 @echo copy done!!
-explorer D:\Software\Dropbox\yilinsendmail\cmdServer.exe
+REM explorer D:\Software\Dropbox\yilinsendmail\cmdServer.exe
 pause
