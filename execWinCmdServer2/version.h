e11734668c8c4bf5290e59467f4658baac70761d
weilaidb <wxjlmr@126.com>
2019-04-24 21:27:18 +0800

    6.6

--- execWinCmdServer2/version.h
+++ execWinCmdServer2/version.h
@@ -1,15 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#define VERSION ("2.2")
-=======
-#define VERSION (2.0)
->>>>>>> parent of 38b3f5b... 6.5修改传输编码为gb2312，以支持jsp。
-=======
-#define VERSION (2.1)
->>>>>>> parent of 81ed008... 状态栏显示内容转成汉字
-
+#define VERSION ("2.4")
 
 #endif // VERSION_H
