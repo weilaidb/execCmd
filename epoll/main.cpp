1969a175b763f81eb38ffd6298929ab118e4dc0c
Your Name <you@example.com>
2020-05-31 12:42:09 +0800

    got useage out

--- epoll/main.cpp
+++ epoll/main.cpp
@@ -39,14 +39,9 @@ void setnonblocking(int sock)
     }
 }
 
-int main(int argc, char* argv[])
+int usage(int argc, char* argv[])
 {
-    int i, maxi, listenfd, connfd, sockfd,epfd,nfds, portnumber;
-    ssize_t n;
-    char line[MAXLINE];
-    socklen_t clilen;
-
-
+    int portnumber;
     if ( 2 == argc )
     {
         if( (portnumber = atoi(argv[1])) < 0 )
@@ -61,7 +56,17 @@ int main(int argc, char* argv[])
         fprintf(stderr,"Usage:%s portnumber\n",argv[0]);
         fprintf(stderr,"Using default portnumber:%d\n",portnumber);
     }
+	return portnumber;
+}
 
+int main(int argc, char* argv[])
+{
+    int i, maxi, listenfd, connfd, sockfd,epfd,nfds, portnumber;
+    ssize_t n;
+    char line[MAXLINE];
+    socklen_t clilen;
+
+	portnumber = usage(argc, argv);
 
     //声明epoll_event结构体的变量,ev用于注册事件,数组用于回传要处理的事件
 
@@ -116,7 +121,6 @@ int main(int argc, char* argv[])
 
         nfds=epoll_wait(epfd,events,DEFAULT_MAXEVENTS,DEFAULT_TIMEOUT);
         //处理所发生的所有事件
-
         for(i=0;i<nfds;++i)
         {
             if(events[i].data.fd==listenfd)//如果新监测到一个SOCKET用户连接到了绑定的SOCKET端口，建立新的连接。
