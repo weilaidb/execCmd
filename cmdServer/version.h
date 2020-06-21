780d405cf9a0ccaa547f31a80efb282aaaa8caee
yjduoduo <wxjlmr@126.com>
2020-06-21 23:40:07 +0800

    变更版本

--- cmdServer/version.h
+++ cmdServer/version.h
@@ -3,6 +3,6 @@
 
 
 //const char *version = "v4.0 Ctrl+F 查找 Ctrl+S 发送 关键字 delete it 删除关键字";
-const char *version = "v7.2 Ctrl+F 查找 Ctrl+S 发送 关键字";
+const char *version = "v7.3 Ctrl+F 查找 Ctrl+S 发送 关键字";
 
 #endif // VERSION_H
